865cf126c806865a551e0c1a5c853bd7a74c89fc
Edwin Fom <edwinfom05@gmail.com>
2024-08-06 16:29:33 +0100

    Updating File

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div>
-      <p>
-        home
-      </p>
-    </div>
-  )
-}
+  const loggedIn = {
+    fistName: "Edwin",
+  };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.fistName || "Guest"}
+            subtext="Access And Manage Your Account
+             and transactions effeciently
+            "
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
