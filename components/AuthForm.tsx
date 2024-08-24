c8f827ec0e564513482334b1fd63eed98e6d6d1f
Edwin Fom <edwinfom05@gmail.com>
2024-08-25 00:57:17 +0100

    Implement the complete Layout

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5"></header>
+    </section>
+  );
+};
+
+export default AuthForm;
